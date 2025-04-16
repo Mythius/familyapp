a723d35acdda02c0415127bea443e1cbc63589c2
matthiassouthwick <southwickmatthias@gmail.com>
2025-04-15 22:32:08 -0600

    birthday calendar

--- public/main.js
+++ public/main.js
@@ -26,16 +26,29 @@ async function main() {
 
 let names, data;
 
+function hideAll() {
+  let elements = document.querySelectorAll(".main");
+  for (let e of elements) {
+    e.classList.add("out");
+  }
+}
+
 function gotoSearch() {
+  hideAll();
   $("search").classList.remove("out");
-  $("#profile").classList.add("out");
   $("#search").value = "";
   renderTable();
 }
 
 function gotoProfile() {
-  $("search").classList.add("out");
+  hideAll();
   $("#profile").classList.remove("out");
+}
+
+function gotoCalendar() {
+  hideAll();
+  $("#calendar").classList.remove("out");
+  populateCalendarMonth();
 }
 
 async function handleClick(name) {
@@ -47,10 +60,76 @@ async function handleClick(name) {
   $("#phone").innerHTML = profile[0].phone;
   $("#email").innerHTML = profile[0].email;
   let birth_date = profile[0].birthday;
+  $("#bday").innerHTML = new Date(profile[0].birthday).toLocaleDateString(
+    "en-us",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+  );
   $("#age").innerHTML = birth_date
-    ? "Age: " + Math.floor((new Date() - new Date(birth_date)) / 31536000000)
+    ? Math.floor((new Date() - new Date(birth_date)) / 31536000000)
     : "Brirthday not found";
+
+  $("#dday").innerHTML =
+    profile.death_date == null
+      ? ""
+      : new Date(profile[0].death_date).toLocaleDateString("en-us", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
 }
+
+function populateCalendarMonth(m = month) {
+  let birthdays_this_month = data
+    .filter((e) => e[7])
+    .filter((e) => new Date(e[7]).getMonth() == m);
+  let first_day_of_month = new Date();
+  first_day_of_month.setMonth(m);
+  first_day_of_month.setDate(1);
+  let last_day_of_month = new Date(first_day_of_month);
+  last_day_of_month.setMonth(last_day_of_month.getMonth() + 1);
+  last_day_of_month.setDate(0);
+  $("#monthlabel").innerHTML = first_day_of_month.toLocaleDateString("en-us", {
+    month: "long",
+  });
+  $("#month").innerHTML = "";
+  let counter = 0,
+    d;
+  for (let row = 0; row < 6; row++) {
+    for (let day = 0; day < 7; day++) {
+      let box = document.createElement("div");
+      box.classList.add("day");
+      d = ++counter - first_day_of_month.getDay();
+      if (d > last_day_of_month.getDate()) d = -1;
+      box.innerHTML += d >= 1 ? d : "";
+      let bdays_today = birthdays_this_month.filter(
+        (e) => new Date(e[7]).getDate() == d
+      );
+      for (let bday of bdays_today) {
+        box.innerHTML += "<hr>" + bday[2];
+      }
+      $("#month").appendChild(box);
+    }
+    if (d == -1) {
+      break;
+    }
+  }
+}
+
+function nextMonth() {
+  month = (month + 1 + 12) % 12;
+  populateCalendarMonth();
+}
+
+function prevMonth() {
+  month = (month - 1 + 12) % 12;
+  populateCalendarMonth();
+}
+
+let month = new Date().getMonth();
 
 function renderTable() {
   const table = document.getElementById("table");
